fadf591ab07bab84f142189843777b4ac2fae1bc
wojtryb <wojtryb@gmail.com>
2020-11-10 21:56:09 +0100

    short and long pressing actions

--- toolModifier/toolModifier.py
+++ toolModifier/toolModifier.py
@@ -1,9 +1,14 @@
 from krita import *
+from time import time
+from .currentTool import getCurrentTool
 
 SHORTCUTS = [
-(Qt.Key_U, "KisToolSelectOutline"),
-(Qt.Key_T, "KritaFill/KisToolGradient")
+(Qt.Key_W, "KisToolSelectOutline"),
+(Qt.Key_S, "KritaFill/KisToolGradient"),
+(Qt.Key_V, "KritaShape/KisToolLine")
 ]
+
+TIMEINTERVAL = 0.3
 
 class toolModifier(Extension):
 
@@ -13,15 +18,49 @@ class toolModifier(Extension):
 	class mdiAreaFilter(QMdiArea):
 		def __init__(self, parent=None):
 			super().__init__(parent)
-			
+			self.updateTime()
+			self.updateDiscardTime(True)
+			self.updateDiscardTime(False)
+			self.state = False
+
+		def updateTime(self):
+			self.t = time()
+
+		def updateDiscardTime(self, par):
+			if par:
+				self.discardTimer1 = time()
+			else:
+				self.discardTimer2 = time()
+
+		def discardByTime(self, par):
+			if par: t = self.discardTimer1
+			else: t = self.discardTimer2
+			if time() - t < 0.05: return True
+			else: return False
+
 		def eventFilter(self, obj, e):
 			if e.type() == QEvent.KeyPress or e.type() == QEvent.KeyRelease:
 				for shortcut in SHORTCUTS:
 					if e.key() == shortcut[0] and not e.isAutoRepeat():
 						if e.type() == QEvent.KeyPress:
-							Application.instance().action(shortcut[1]).trigger()
+							if not self.discardByTime(True):
+								if getCurrentTool() != shortcut[1]:
+									Application.instance().action(shortcut[1]).trigger()
+								else:
+									Application.instance().action("KritaShape/KisToolBrush").trigger()
+								self.updateTime()
+								self.updateDiscardTime(True)
+
 						elif e.type() == QEvent.KeyRelease:
-							Application.instance().action("KritaShape/KisToolBrush").trigger()
+							if not self.discardByTime(False):
+
+								if time() - self.t > TIMEINTERVAL:
+									if getCurrentTool() == shortcut[1]:
+										Application.instance().action("KritaShape/KisToolBrush").trigger()
+
+								self.updateDiscardTime(False)
+
+						break
 			return False
 
 	def setup(self):
@@ -33,6 +72,5 @@ class toolModifier(Extension):
 
 		self.mdiAreaFilter = self.mdiAreaFilter()
 		self.mdiArea.installEventFilter(self.mdiAreaFilter)
-		print("created")
 
 Krita.instance().addExtension(toolModifier(Krita.instance()))
