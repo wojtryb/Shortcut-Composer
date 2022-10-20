f10e3e5ba2ee7a87a04226a712c1bbcee619e739
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-10-20 20:20:26 +0200

    Made action classes clearer

--- toolModifiers/importCode/actions.py
+++ toolModifiers/importCode/actions.py
@@ -1,14 +1,9 @@
 from krita import Krita
-from .current_tool import get_current_tool_name
 from abc import ABC
-
 from dataclasses import dataclass
+
+from .current_tool import get_current_tool_name
 from ..config import connected_toggles
-
-
-def set_tool(tool_name):
-    'activates a tool of passed name'
-    Krita.instance().action(tool_name).trigger()
 
 
 class Action(ABC):
@@ -31,21 +26,19 @@ class ToolWrapper(Action):
     human_name: str
 
     def set_low(self):
-        set_tool("KritaShape/KisToolBrush")
+        self._set_tool("KritaShape/KisToolBrush")
 
     def set_high(self):
-        set_tool(self.krita_name)
+        self._set_tool(self.krita_name)
 
     def is_high_state(self):
         'returns True if the passed tool is active'
         return get_current_tool_name() == self.krita_name
 
-
-def toggle_eraser():
-    'changes the state of the eraser, may affect alpha lock'
-    if connected_toggles:
-        Krita.instance().action("preserve_alpha").setChecked(False)
-    Krita.instance().action("erase_action").trigger()
+    @staticmethod
+    def _set_tool(tool_name):
+        'activates a tool of passed name'
+        Krita.instance().action(tool_name).trigger()
 
 
 class EraserWrapper(Action):
@@ -54,22 +47,22 @@ class EraserWrapper(Action):
         self.human_name = 'Eraser (toggle)'
 
     def set_low(self):
-        toggle_eraser()
+        self._toggle_eraser()
 
     def set_high(self):
-        toggle_eraser()
+        self._toggle_eraser()
 
     def is_high_state(self):
         'returns True if the passed tool is active'
         krita_eraser_action = Krita.instance().action("erase_action")
         return krita_eraser_action.isChecked()
 
-
-def toggle_alpha_lock():
-    'changes the state of the alpha lock, may affect the eraser'
-    if connected_toggles:
-        Krita.instance().action("erase_action").setChecked(False)
-    Krita.instance().action("preserve_alpha").trigger()
+    @staticmethod
+    def _toggle_eraser():
+        'changes the state of the eraser, may affect alpha lock'
+        if connected_toggles:
+            Krita.instance().action("preserve_alpha").setChecked(False)
+        Krita.instance().action("erase_action").trigger()
 
 
 class AlphaWrapper(Action):
@@ -78,12 +71,19 @@ class AlphaWrapper(Action):
         self.human_name = 'Preserve alpha (toggle)'
 
     def set_low(self):
-        toggle_alpha_lock()
+        self._toggle_alpha_lock()
 
     def set_high(self):
-        toggle_alpha_lock()
+        self._toggle_alpha_lock()
 
     def is_high_state(self):
         'returns True if the alpha is locked'
         krita_preserve_alpha_action = Krita.instance().action("preserve_alpha")
         return krita_preserve_alpha_action.isChecked()
+
+    @staticmethod
+    def _toggle_alpha_lock():
+        'changes the state of the alpha lock, may affect the eraser'
+        if connected_toggles:
+            Krita.instance().action("erase_action").setChecked(False)
+        Krita.instance().action("preserve_alpha").trigger()
