5e7cdb65969cd9d062bf43bbe9266bbd6244e068
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-11-17 11:29:47 +0100

    Reverted preset choser from slider to multiple assignment - cant be threaded

--- shortcut_composer/data_components/tag.py
+++ shortcut_composer/data_components/tag.py
@@ -1,7 +1,7 @@
 from api_krita.wrappers import Database
 
 
-class Tag:
+class Tag(list):
     """
     List-like container representating presets in a tag.
 
@@ -13,16 +13,5 @@ class Tag:
         self.name = name
         with Database() as database:
             preset_names = database.get_preset_names_from_tag(name)
-        self.data = sorted(set(preset_names))
-
-    def __iter__(self):
-        return iter(self.data)
-
-    def __getitem__(self, index: int):
-        return self.data[index]
-
-    def __len__(self):
-        return len(self.data)
-
-    def index(self, value: str):
-        return self.data.index(value)
+        self.extend(set(preset_names))
+        self.sort()
