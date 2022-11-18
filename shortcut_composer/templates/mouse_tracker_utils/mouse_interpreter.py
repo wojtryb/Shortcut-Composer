157dff4cfa5e0acd5f01f2abc0e9772e4f336204
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-11-18 15:11:53 +0100

    Rewritten MouseInterpreter

--- shortcut_composer/templates/mouse_tracker_utils/mouse_interpreter.py
+++ shortcut_composer/templates/mouse_tracker_utils/mouse_interpreter.py
@@ -13,21 +13,24 @@ class MouseInterpreter:
     pixels_in_unit: float
 
     def interpret(self, mouse: MouseInput) -> Interpreted:
-        value = self.start_value + self._delta(mouse)
-        self._recalibrate(value)
-        return self._clip(value)
+        mouse_delta = MouseInput(mouse - self.mouse_origin)
+        value_delta = self.mouse_to_value(mouse_delta)
+        raw_value = Interpreted(self.start_value + value_delta)
+        self._recalibrate_mouse_origin(raw_value)
+        return self._clip(raw_value)
 
-    def _recalibrate(self, value: float) -> None:
-        delta = (self.min - value)*self.pixels_in_unit
-        if delta > 0:
-            self.mouse_origin = MouseInput(round(self.mouse_origin - delta))
+    def _recalibrate_mouse_origin(self, value: Interpreted) -> None:
+        if (middle := self._clip(value)) == value:
+            return
+        value_delta = Interpreted(middle - value)
+        mouse_delta = self.interpreted_to_mouse(value_delta)
+        self.mouse_origin = MouseInput(self.mouse_origin - mouse_delta)
 
-        delta = (self.max - value)*self.pixels_in_unit
-        if delta < 0:
-            self.mouse_origin = MouseInput(round(self.mouse_origin - delta))
+    def mouse_to_value(self, mouse: MouseInput) -> Interpreted:
+        return Interpreted(mouse/self.pixels_in_unit)
 
-    def _delta(self, mouse: float) -> float:
-        return (mouse - self.mouse_origin)/self.pixels_in_unit
+    def interpreted_to_mouse(self, value: Interpreted) -> MouseInput:
+        return MouseInput(round(value*self.pixels_in_unit))
 
-    def _clip(self, value: float) -> Interpreted:
-        return Interpreted(min(max(self.min, value), self.max))
+    def _clip(self, value: Interpreted) -> Interpreted:
+        return sorted((self.min, value, self.max))[1]
