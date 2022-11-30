8d5bdc38d772c2dcca1d364a5944a21268b1a2f0
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-11-30 14:49:54 +0100

    Displaying tool Icons

--- shortcut_composer/templates/pie_menu.py
+++ shortcut_composer/templates/pie_menu.py
@@ -21,7 +21,7 @@ from api_krita import Krita
 T = TypeVar('T')
 
 
-def mask_image(image: QImage, imgtype='jpg', size=64):
+def mask_image(image: QImage, size=100):
     image.convertToFormat(QImage.Format_ARGB32)
 
     imgsize = min(image.width(), image.height())
@@ -90,8 +90,7 @@ class MyWidget(QWidget):
         label.show()
 
     def _paint_preset_label(self):
-        label = QLabel("text label", self)
-        label.setFont(QFont('Times', 20))
+        label = QLabel("paint label", self)
         label.adjustSize()
         label.setGeometry(500, 100, 100, 100)
         label.setStyleSheet(
@@ -99,11 +98,26 @@ class MyWidget(QWidget):
             "color: white;"
         )
         label.setAlignment(Qt.AlignCenter)
-        label.setWordWrap(True)
-
         presets = Krita.get_presets()
         image = list(presets.values())[5].image()
-        pixels = mask_image(image)
+        pixels = mask_image(image, size=100)
+        label.setPixmap(pixels)
+
+        label.show()
+
+    def _paint_icon_label(self):
+        label = QLabel("icon label", self)
+        label.adjustSize()
+        label.setGeometry(500, 300, 100, 100)
+        label.setStyleSheet(
+            "background-color:rgba(47, 47, 47, 255);"
+            "color: white;"
+            "border-radius: 50px;"
+            "border: 3px rgba(60, 60, 60, 255);"
+        )
+        label.setAlignment(Qt.AlignCenter)
+        icon = Krita.get_icon("tool_crop")
+        pixels = icon.pixmap(60, 60)
         label.setPixmap(pixels)
 
         label.show()
@@ -115,6 +129,7 @@ class MyWidget(QWidget):
         self._paint_wheel()
         self._paint_text_label()
         self._paint_preset_label()
+        self._paint_icon_label()
         self.painter.end()
 
 
