9f34b2bdc87cb90dd128f8c16871400857e4fe7c
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-12-05 14:23:59 +0100

    Small code tweaks

--- shortcut_composer/templates/pie_menu_utils/pie_style.py
+++ shortcut_composer/templates/pie_menu_utils/pie_style.py
@@ -6,8 +6,12 @@ from PyQt5.QtGui import QColor
 class PieStyle:
     pie_radius: int
     icon_radius: int
-    widget_radius: int
-    border_thickness: int
-    area_thickness: int
-    color: QColor = QColor(55, 55, 55, 230)
+    area_color: QColor
+
+    icon_color: QColor = QColor(75, 75, 75, 255)
     border_color: QColor = QColor(55, 55, 55, 255)
+
+    def __post_init__(self):
+        self.widget_radius = self.pie_radius + self.icon_radius
+        self.border_thickness = round(self.icon_radius*0.1)
+        self.area_thickness = round(self.pie_radius*0.4)
