28285366074931ade87b396ef253891a2265ad4f
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-12-12 15:48:00 +0100

    Handle empty pie menus

--- shortcut_composer/templates/pie_menu_utils/settings_dialog.py
+++ shortcut_composer/templates/pie_menu_utils/settings_dialog.py
@@ -17,7 +17,7 @@ class SettingsDialog(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
-        self.setMinimumSize(QSize(300, 150))
+        self.setMinimumSize(QSize(300, 200))
         self.setWindowTitle("Shortcut composer settings")
 
         self.combo_boxes: List[QComboBox] = []
@@ -34,11 +34,13 @@ class SettingsDialog(QWidget):
     def _create_button_layout(self):
         buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
 
-        button_box = QDialogButtonBox(buttons)
+        button_box = QDialogButtonBox(buttons)  # type: ignore
         button_box.accepted.connect(self.handle_ok_button)
         button_box.rejected.connect(self.handle_cancel_button)
 
         button_layout = QVBoxLayout()
+        button_layout.addWidget(QLabel(
+            "Selected tags gets updated after krita restart."))
         button_layout.addWidget(button_box)
         button_layout.setAlignment(Qt.AlignBottom)
 
@@ -48,11 +50,12 @@ class SettingsDialog(QWidget):
         tags = self._get_tags()
 
         combo_layout = QGridLayout()
-        combo_layout.setRowStretch(0, 60)
         combo_layout.setAlignment(Qt.AlignTop)
 
         def add_row(name: str, row_id: int):
-            combo_layout.addWidget(QLabel(name), row_id, 0)
+            label = QLabel(name)
+            label.setFixedWidth(100)
+            combo_layout.addWidget(label, row_id, 0)
             combo_box = self._create_combobox(name, tags)
             combo_layout.addWidget(combo_box, row_id, 1)
 
