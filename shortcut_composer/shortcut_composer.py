061706d941450cd9218588deb802353b50bba2fe
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-12-13 13:52:02 +0100

    Creating actions with api_krita

--- shortcut_composer/shortcut_composer.py
+++ shortcut_composer/shortcut_composer.py
@@ -1,6 +1,6 @@
 """Main file which defines extension class and adds it to krita."""
 
-from api_krita import Extension  # type: ignore
+from api_krita import Krita, Extension  # type: ignore
 from input_adapter import ActionManager
 from .actions import create_actions
 from .composer_utils import SettingsDialog
@@ -16,9 +16,17 @@ class ShortcutComposer(Extension):
 
     def createActions(self, window) -> None:
         """Create ActionManager which holds and binds them to krita."""
-        self._pie_setting_dialog = SettingsDialog()
-        self._settings_action = self._create_settings(window)
-        self._reload_action = self._create_reload(window)
+        self._pie_settings_dialog = SettingsDialog()
+
+        self._settings_action = Krita.create_action(
+            window=window,
+            name="Shortcut Composer Settings",
+            group="tools/scripts",
+            callback=self._pie_settings_dialog.show)
+        self._reload_action = Krita.create_action(
+            window=window,
+            name="Reload Shortcut Composer",
+            callback=self.reload_composer)
 
         self._manager = ActionManager(window)
         self.reload_composer()
@@ -26,21 +34,3 @@ class ShortcutComposer(Extension):
     def reload_composer(self):
         for action in create_actions():
             self._manager.bind_action(action)
-
-    def _create_settings(self, window):
-        settings_action = window.createAction(
-            "Shortcut Composer Settings",
-            "Shortcut Composer Settings",
-            "tools/scripts")
-        settings_action.setAutoRepeat(False)
-        settings_action.triggered.connect(self._pie_setting_dialog.show)
-        return settings_action
-
-    def _create_reload(self, window):
-        reload_action = window.createAction(
-            "Reload Shortcut Composer",
-            "Reload Shortcut Composer",
-            "")
-        reload_action.setAutoRepeat(False)
-        reload_action.triggered.connect(self.reload_composer)
-        return reload_action
