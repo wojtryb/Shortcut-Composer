5ca9f01a6ff6dd558e8bddd74ef231a62ef2fd80
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-12-13 14:11:12 +0100

    Refreshing tag comboboxes

--- shortcut_composer/composer_utils/settings_dialog.py
+++ shortcut_composer/composer_utils/settings_dialog.py
@@ -35,8 +35,8 @@ class SettingsDialog(QWidget):
         buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
 
         button_box = QDialogButtonBox(buttons)  # type: ignore
-        button_box.accepted.connect(self.handle_ok_button)
-        button_box.rejected.connect(self.handle_cancel_button)
+        button_box.accepted.connect(self._handle_ok_button)
+        button_box.rejected.connect(self._handle_cancel_button)
 
         button_layout = QVBoxLayout()
         button_layout.addWidget(QLabel(
@@ -46,26 +46,7 @@ class SettingsDialog(QWidget):
 
         return button_layout
 
-    def _create_combobox_layout(self):
-        tags = self._get_tags()
-
-        combo_layout = QGridLayout()
-        combo_layout.setAlignment(Qt.AlignTop)
-
-        def add_row(name: str, row_id: int):
-            label = QLabel(name)
-            label.setFixedWidth(100)
-            combo_layout.addWidget(label, row_id, 0)
-            combo_box = self._create_combobox(name, tags)
-            combo_layout.addWidget(combo_box, row_id, 1)
-
-        add_row("Tag (green)", 0)
-        add_row("Tag (blue)", 1)
-        add_row("Tag (red)", 2)
-
-        return combo_layout
-
-    def handle_ok_button(self):
+    def _handle_ok_button(self):
         for combo in self.combo_boxes:
             Krita.write_setting(
                 group="ShortcutComposer",
@@ -75,25 +56,46 @@ class SettingsDialog(QWidget):
             Krita.trigger_action("Reload Shortcut Composer")
         self.hide()
 
-    def handle_cancel_button(self):
+    def _handle_cancel_button(self):
         self.hide()
 
-    def _get_tags(self):
-        with Database() as database:
-            return database.get_brush_tags()
+    def _create_combobox_layout(self):
+        combo_layout = QGridLayout()
+        combo_layout.setAlignment(Qt.AlignTop)
 
-    def _create_combobox(self, name: str, tags: List[str]):
+        def add_row(name: str, row_id: int):
+            label = QLabel(name)
+            label.setFixedWidth(100)
+            combo_layout.addWidget(label, row_id, 0)
+            combo_box = self._create_combobox(name)
+            combo_layout.addWidget(combo_box, row_id, 1)
+
+        add_row("Tag (green)", 0)
+        add_row("Tag (blue)", 1)
+        add_row("Tag (red)", 2)
+
+        return combo_layout
+
+    def _create_combobox(self, name: str):
         combo_box = QComboBox(self)
         combo_box.setObjectName(name)
-        combo_box.addItems(tags)
-        combo_box.setCurrentText(Krita.read_setting(
-            group="ShortcutComposer",
-            name=name,
-            default="RGBA",
-        ))
         self.combo_boxes.append(combo_box)
         return combo_box
 
     def show(self) -> None:
+        self._refresh_comboboxes()
         self.move(QCursor.pos())
         return super().show()
+
+    def _refresh_comboboxes(self):
+        with Database() as database:
+            tags = database.get_brush_tags()
+
+        for combo_box in self.combo_boxes:
+            combo_box.clear()
+            combo_box.addItems(sorted(tags))
+            combo_box.setCurrentText(Krita.read_setting(
+                group="ShortcutComposer",
+                name=combo_box.objectName(),
+                default="RGBA",
+            ))
