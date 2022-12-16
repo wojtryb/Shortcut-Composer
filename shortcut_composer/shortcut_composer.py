5e10d83595502e7b97ecb15cd750553d243c17b5
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2022-12-16 13:21:54 +0100

    Further refactoring transform modes

--- shortcut_composer/shortcut_composer.py
+++ shortcut_composer/shortcut_composer.py
@@ -21,8 +21,10 @@ class ShortcutComposer(Extension):
 
     def createActions(self, window) -> None:
         """Create ActionManager which holds and binds them to krita."""
-        self._pie_settings_dialog = SettingsDialog()
+        self._transform_modes = TransformModeActions()
+        self._transform_modes.create_actions(window)
 
+        self._pie_settings_dialog = SettingsDialog()
         self._settings_action = Krita.create_action(
             window=window,
             name="Shortcut Composer Settings",
@@ -32,29 +34,6 @@ class ShortcutComposer(Extension):
             window=window,
             name="Reload Shortcut Composer",
             callback=self.reload_composer)
-
-        self._transform_modes = TransformModeActions()
-
-        self._perspective_action = Krita.create_action(
-            window=window,
-            name="Transform tool: perspective",
-            callback=self._transform_modes.set_perspective)
-        self._warp_action = Krita.create_action(
-            window=window,
-            name="Transform tool: warp",
-            callback=self._transform_modes.set_warp)
-        self._cage_action = Krita.create_action(
-            window=window,
-            name="Transform tool: cage",
-            callback=self._transform_modes.set_cage)
-        self._liquify_action = Krita.create_action(
-            window=window,
-            name="Transform tool: liquify",
-            callback=self._transform_modes.set_liquify)
-        self._mesh_action = Krita.create_action(
-            window=window,
-            name="Transform tool: mesh",
-            callback=self._transform_modes.set_mesh)
 
         self._manager = ActionManager(window)
         self.reload_composer()
