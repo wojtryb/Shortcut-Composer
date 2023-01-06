db619c9b331f659bf47f85b9fef0f055b3244c98
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Divided transform modes into two classes

--- shortcut_composer/composer_utils/transform_actions.py
+++ shortcut_composer/composer_utils/transform_actions.py
@@ -15,69 +15,23 @@ from api_krita import Krita
 
 
 class TransformModeActions:
-    """Helper class for finding currently active tool."""
 
     def __init__(self) -> None:
-        """Remember the reference to toolbox krita object."""
+        self._finder = self.TransformModeFinder()
         self._actions: Dict[str, QWidgetAction] = {}
-        self._tool_buttons: Dict[Tool, QToolButton] = {}
-        self._initialized = False
-
-        self._transform_options: QWidget
-        self._apply_button: QPushButton
-
-    def _ensure_initialized(self, tool: Tool):
-        if not self._initialized:
-            Tool.TRANSFORM.activate()
-            self._transform_options = self._fetch_transform_options()
-            self._apply_button = self._fetch_apply_button()
-            self._initialized = True
-
-        if tool not in self._tool_buttons:
-            self._tool_buttons[tool] = self._fetch_tool_button(tool)
 
     def _set_mode(self, tool: Tool) -> None:
-        self._ensure_initialized(tool)
-        button = self._tool_buttons[tool]
+        self._finder.ensure_initialized(tool)
 
         if Krita.active_tool == Tool.TRANSFORM:
-            return self._activate_mode_button(button)
+            return self._finder.activate_tool(tool)
 
         Tool.TRANSFORM.activate()
-        Thread(target=self._delayed_click, args=[button], daemon=True).start()
+        Thread(target=self._delayed_click, args=[tool], daemon=True).start()
 
-    def _delayed_click(self, button: QToolButton):
+    def _delayed_click(self, tool: Tool):
         sleep(0.1)
-        self._activate_mode_button(button)
-
-    def _activate_mode_button(self, button: QToolButton):
-        self._apply_button.click()
-        button.click()
-
-    set_perspective = partialmethod(_set_mode, Tool.TRANSFORM_PERSPECTIVE)
-    set_warp = partialmethod(_set_mode, Tool.TRANSFORM_WARP)
-    set_cage = partialmethod(_set_mode, Tool.TRANSFORM_CAGE)
-    set_liquify = partialmethod(_set_mode, Tool.TRANSFORM_LIQUIFY)
-    set_mesh = partialmethod(_set_mode, Tool.TRANSFORM_MESH)
-
-    def _fetch_tool_button(self, tool: Tool) -> QToolButton:
-        for qobj in self._transform_options.findChildren(QToolButton):
-            if qobj.objectName() == self._BUTTONS_MAP[tool]:
-                return qobj  # type: ignore
-        raise RuntimeError("Couldnt find tool button.")
-
-    def _fetch_apply_button(self) -> QPushButton:
-        for qobj in self._transform_options.findChildren(QPushButton):
-            if qobj.text() == "&Apply":
-                return qobj  # type: ignore
-        raise RuntimeError("Couldnt find apply button.")
-
-    def _fetch_transform_options(self) -> QWidget:
-        """Find and return reference to unwrapped options object."""
-        for qobj in Krita.get_active_qwindow().findChildren(QWidget):
-            if qobj.objectName() == "KisToolTransform option widget":
-                return qobj  # type: ignore
-        raise RuntimeError("Transform options not found.")
+        self._finder.activate_tool(tool)
 
     def create_actions(self, window):
         _ACTION_MAP = {
@@ -95,10 +49,57 @@ class TransformModeActions:
                 callback=implementation
             )
 
-    _BUTTONS_MAP = {
-        Tool.TRANSFORM_PERSPECTIVE: "perspectiveTransformButton",
-        Tool.TRANSFORM_WARP: "warpButton",
-        Tool.TRANSFORM_CAGE: "cageButton",
-        Tool.TRANSFORM_LIQUIFY: "liquifyButton",
-        Tool.TRANSFORM_MESH: "meshButton",
-    }
+    set_perspective = partialmethod(_set_mode, Tool.TRANSFORM_PERSPECTIVE)
+    set_warp = partialmethod(_set_mode, Tool.TRANSFORM_WARP)
+    set_cage = partialmethod(_set_mode, Tool.TRANSFORM_CAGE)
+    set_liquify = partialmethod(_set_mode, Tool.TRANSFORM_LIQUIFY)
+    set_mesh = partialmethod(_set_mode, Tool.TRANSFORM_MESH)
+
+    class TransformModeFinder:
+        def __init__(self) -> None:
+            """Remember the reference to toolbox krita object."""
+            self.tool_buttons: Dict[Tool, QToolButton] = {}
+            self._initialized = False
+
+            self._transform_options: QWidget
+            self._apply_button: QPushButton
+
+        def ensure_initialized(self, tool: Tool):
+            if not self._initialized:
+                Tool.TRANSFORM.activate()
+                self._transform_options = self._fetch_transform_options()
+                self._apply_button = self._fetch_apply_button()
+                self._initialized = True
+
+            if tool not in self.tool_buttons:
+                self.tool_buttons[tool] = self._fetch_tool_button(tool)
+
+        def activate_tool(self, tool: Tool):
+            self._apply_button.click()
+            self.tool_buttons[tool].click()
+
+        def _fetch_tool_button(self, tool: Tool) -> QToolButton:
+            for qobj in self._transform_options.findChildren(QToolButton):
+                if qobj.objectName() == self._BUTTONS_MAP[tool]:
+                    return qobj  # type: ignore
+            raise RuntimeError("Couldnt find tool button.")
+
+        def _fetch_apply_button(self) -> QPushButton:
+            for qobj in self._transform_options.findChildren(QPushButton):
+                if qobj.text() == "&Apply":
+                    return qobj  # type: ignore
+            raise RuntimeError("Couldnt find apply button.")
+
+        def _fetch_transform_options(self) -> QWidget:
+            for qobj in Krita.get_active_qwindow().findChildren(QWidget):
+                if qobj.objectName() == "KisToolTransform option widget":
+                    return qobj  # type: ignore
+            raise RuntimeError("Transform options not found.")
+
+        _BUTTONS_MAP = {
+            Tool.TRANSFORM_PERSPECTIVE: "perspectiveTransformButton",
+            Tool.TRANSFORM_WARP: "warpButton",
+            Tool.TRANSFORM_CAGE: "cageButton",
+            Tool.TRANSFORM_LIQUIFY: "liquifyButton",
+            Tool.TRANSFORM_MESH: "meshButton",
+        }
