f676cf51d1f43cd908d15ef51c84d1783b53c8c7
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    POC for drawing pies

--- shortcut_composer/api_krita/pyqt/painter.py
+++ shortcut_composer/api_krita/pyqt/painter.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from PyQt5.QtGui import QPainter, QPainterPath, QColor, QPixmap
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import QPoint
+from PyQt5.QtCore import QPoint, QRectF
 
 
 class Painter:
@@ -31,6 +31,21 @@ class Painter:
             center.y() - pixmap.height()//2
         )
         self._painter.drawPixmap(left_top_corner, pixmap)
+
+    def paint_pie(self, center: QPoint, angle: int, span: int, color: QColor):
+        path = QPainterPath()
+        path.moveTo(center)
+        start = 10
+        length = 45
+        radius = 50
+        rectangle = QRectF(center.x()-radius, center.y()-radius, radius*2, radius*2)
+        path.arcTo(rectangle, start, length)
+
+        radius = 25
+        rectangle = QRectF(center.x()-radius, center.y()-radius, radius*2, radius*2)
+        path.arcTo(rectangle, start+length, -length)
+
+        self._painter.fillPath(path, color)
 
     def end(self):
         self._painter.end()
