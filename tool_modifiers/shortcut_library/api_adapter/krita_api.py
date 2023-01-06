ae74169a43232a16be0dd5a68cf5a50beb6ba3df
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Api wrapper to canvas

--- tool_modifiers/shortcut_library/api_adapter/krita_api.py
+++ tool_modifiers/shortcut_library/api_adapter/krita_api.py
@@ -109,6 +109,24 @@ class KritaDocument:
         self.document.refreshProjection()
 
 
+@dataclass
+class KritaCanvas:
+
+    canvas: Any
+
+    def rotation(self):
+        return self.canvas.rotation()
+
+    def set_rotation(self, angle_deg: float):
+        self.canvas.setRotation(angle_deg)
+
+    def zoom(self):
+        return self.canvas.zoomLevel()
+
+    def set_zoom(self, zoom: int):
+        self.canvas.setZoomLevel(zoom)
+
+
 class Krita:
 
     @staticmethod
@@ -123,6 +141,10 @@ class Krita:
     @staticmethod
     def get_active_document() -> KritaDocument:
         return KritaDocument(Api.instance().activeDocument())
+
+    @staticmethod
+    def get_active_canvas() -> KritaCanvas:
+        return KritaCanvas(Api.instance().activeWindow().activeView().canvas())
 
     @staticmethod
     def trigger_action(action_name) -> None:
