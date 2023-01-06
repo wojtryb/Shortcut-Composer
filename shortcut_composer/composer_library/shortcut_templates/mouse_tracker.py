e31ac99fcc076c8f093d5d09168b21e37bf868c2
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Fix tracker freezing GUI thread on slider choosing

--- shortcut_composer/composer_library/shortcut_templates/mouse_tracker.py
+++ shortcut_composer/composer_library/shortcut_templates/mouse_tracker.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from threading import Thread
 
 from time import sleep
 from typing import Optional
@@ -19,16 +20,20 @@ class MouseTracker(PluginAction):
     working = False
 
     def on_key_press(self) -> None:
-        self.working = True
         cursor = Krita.get_cursor()
 
         if self.horizontal_slider and not self.vertical_slider:
             return self.horizontal_slider.start(cursor.x)
         if not self.horizontal_slider and self.vertical_slider:
             return self.vertical_slider.start(lambda: -cursor.y())
+        if not self.horizontal_slider and not self.vertical_slider:
+            return
+        Thread(target=self._pick_slider, daemon=True).start()
 
+    def _pick_slider(self):
+        cursor = Krita.get_cursor()
         start_point = (cursor.x(), cursor.y())
-        while self.working:
+        while True:
             delta_hor = abs(start_point[0] - cursor.x())
             delta_ver = abs(start_point[1] - cursor.y())
             if abs(delta_hor - delta_ver) >= 10:
@@ -41,7 +46,6 @@ class MouseTracker(PluginAction):
             self.vertical_slider.start(lambda: -cursor.y())
 
     def on_every_key_release(self) -> None:
-        self.working = False
         if self.horizontal_slider:
             self.horizontal_slider.stop()
         if self.vertical_slider:
