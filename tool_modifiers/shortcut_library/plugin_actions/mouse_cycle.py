582eed28d93321bd249c3c074a91adfa7ca5f796
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Contignious mouse handler

--- tool_modifiers/shortcut_library/plugin_actions/mouse_cycle.py
+++ tool_modifiers/shortcut_library/plugin_actions/mouse_cycle.py
@@ -1,58 +1,65 @@
-from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from threading import Thread
 from time import sleep
+from typing import Union
 
 from .krita_api_wrapper import Krita
 from .interfaces import PluginAction
-from .controllers import Controller
-
-
-class TwoWayIterator(ABC):
-    @abstractmethod
-    def next(self):
-        pass
-
-    @abstractmethod
-    def previous(self):
-        pass
-
-
-class ActionIterator(TwoWayIterator):
-    def next(self):
-        Krita.trigger_action("next_favorite_preset")
-        # Krita.trigger_action("KritaShape/KisToolBrush")
-
-    def previous(self):
-        Krita.trigger_action("previous_favorite_preset")
-        # Krita.trigger_action("KisToolSelectOutline")
-
-
-class Handler:
-    # def __init__(self, controller: Controller, prev_next: TwoWayIterator):
-    #     self.__controller = controller
-    #     self.__two_way_iterator = prev_next
-    def __init__(self):
-        self.__controller = Controller()
-        self.__two_way_iterator = ActionIterator()
-
-        self.__start_value = 0
-        self.__step = 50
-
-    def set_start_value(self, value: int):
-        self.__start_value = value
-
-    def update(self, current_value):
-        if current_value > self.__start_value + self.__step:
-            self.__controller.set_value(self.__two_way_iterator.next())
-            self.__start_value = current_value + self.__step
-
-        elif current_value < self.__start_value - self.__step:
-            self.__controller.set_value(self.__two_way_iterator.previous())
-            self.__start_value = current_value - self.__step
 
 
 @dataclass
+class Range:
+    min: float
+    max: float
+
+
+class ValueInterpreter:
+    def __init__(
+        self,
+        values_to_cycle: Union[list, Range],
+        sensitivity: float
+    ):
+        if isinstance(values_to_cycle, list):
+            self.min = 0
+            self.max = len(values_to_cycle) * sensitivity
+            self.map = {i*sensitivity: value
+                        for i, value in enumerate(values_to_cycle)}
+        elif isinstance(values_to_cycle, Range):
+            self.min = values_to_cycle.min
+            self.max = values_to_cycle.max
+        else:
+            raise RuntimeError(f"Wrong type: {values_to_cycle}")
+
+    def __getitem__(self, item):
+        for key, value in reversed(self.map.items()):
+            if item >= key:
+                return value
+        return value
+
+
+class Handler:
+    def __init__(self, controller,  values_to_cycle, sensitivity=50):
+        self.__controller = controller
+        self.__interpreter = ValueInterpreter(values_to_cycle, sensitivity)
+        self.__current_value = 0
+
+    def set_start_value(self, mouse_value: int):
+        self.__start_value = mouse_value - self.__current_value
+
+    def update(self, mouse_value: int):
+        self.__current_value = mouse_value - self.__start_value
+        to_set = self.__interpreter[self.__current_value]
+        self.__controller.set_value(to_set)
+
+        print(self.__current_value)
+
+        if self.__current_value < self.__interpreter.min:
+            self.__start_value -= self.__interpreter.min - self.__current_value
+        elif self.__current_value > self.__interpreter.max:
+            self.__start_value += self.__current_value - self.__interpreter.max
+
+
+@ dataclass
 class MouseCycle(PluginAction):
 
     action_name: str
@@ -77,7 +84,6 @@ class MouseCycle(PluginAction):
         while self.working:
             self.horizontal_handler.update(qwin.cursor().pos().x())
             self.vertical_handler.update(qwin.cursor().pos().y())
-            # print(qwin.cursor().pos().x())
             sleep(0.05)
 
     def on_every_key_release(self):
