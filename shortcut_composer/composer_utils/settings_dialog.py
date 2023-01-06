75b159a98d2ce76d8c0373dc15d680ee5441bb98
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Resetting and applying changes in settings

--- shortcut_composer/composer_utils/settings_dialog.py
+++ shortcut_composer/composer_utils/settings_dialog.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Dict, Union
 from PyQt5.QtWidgets import (
     QDialogButtonBox,
     QDoubleSpinBox,
@@ -20,6 +20,7 @@ from .krita_setting import read_setting, write_setting
 
 
 class SettingsDialog(QWidget):
+
     def __init__(self):
         QWidget.__init__(self)
 
@@ -27,7 +28,7 @@ class SettingsDialog(QWidget):
         self.setWindowTitle("Shortcut composer settings")
 
         self.combo_boxes: List[QComboBox] = []
-        self.forms: List[Union[QSpinBox, QDoubleSpinBox]] = []
+        self.forms: Dict[Config, Union[QSpinBox, QDoubleSpinBox]] = {}
 
         combo_layout = self._create_combobox_layout()
         form_layout = self._create_form_layout()
@@ -78,49 +79,68 @@ class SettingsDialog(QWidget):
         add_row(Config.PIE_ICON_GLOBAL_SCALE, is_int=False)
         add_row(Config.PIE_DEADZONE_GLOBAL_SCALE, is_int=False)
 
+        self._refresh_forms()
         return form_layout
 
-    def _create_form(self, config: Config, is_int: bool):
+    def _refresh_forms(self):
+        for config, form in self.forms.items():
+            form.setValue(float(config.get()))  # type: ignore
 
+    def _create_form(self, config: Config, is_int: bool):
         form = QSpinBox() if is_int else QDoubleSpinBox()
         form.setObjectName(config.value)
         form.setMinimum(0)
         form.setSingleStep(1 if is_int else 0.1)  # type: ignore
-        form.setValue(float(config.get()))  # type: ignore
 
-        self.forms.append(form)
+        self.forms[config] = form
         return form
 
     def _create_ending_layout(self):
-        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        buttons = (
+            QDialogButtonBox.Ok |
+            QDialogButtonBox.Apply |
+            QDialogButtonBox.Reset |
+            QDialogButtonBox.Cancel
+        )
 
-        button_box = QDialogButtonBox(buttons)  # type: ignore
-        button_box.accepted.connect(self._handle_ok_button)
-        button_box.rejected.connect(self.hide)
+        self.button_box = QDialogButtonBox(buttons)  # type: ignore
+        self.button_box.clicked.connect(self._handle_click)
 
         button_layout = QVBoxLayout()
-        button_layout.addWidget(QLabel("Pressing ok reloads the plugin."))
-        button_layout.addWidget(button_box)
+        button_layout.addWidget(self.button_box)
         button_layout.setAlignment(Qt.AlignBottom)
 
         return button_layout
 
-    def _handle_ok_button(self):
+    def _handle_click(self, button):
+        role = self.button_box.buttonRole(button)
+        if role == QDialogButtonBox.ApplyRole:
+            self._apply_button()
+        elif role == QDialogButtonBox.ResetRole:
+            Config.reset_defaults()
+            self._refresh_forms()
+        elif role == QDialogButtonBox.AcceptRole:
+            self._apply_button()
+            self.hide()
+        elif role == QDialogButtonBox.RejectRole:
+            self.hide()
+
+    def _apply_button(self):
         for combo in self.combo_boxes:
             write_setting(
                 name=combo.objectName(),
                 value=combo.currentText()
             )
-        for form in self.forms:
+        for form in self.forms.values():
             write_setting(
                 name=form.objectName(),
                 value=form.value()
             )
         Krita.trigger_action("Reload Shortcut Composer")
-        self.hide()
 
     def show(self) -> None:
         self._refresh_comboboxes()
+        self._refresh_forms()
         self.move(QCursor.pos())
         return super().show()
 
