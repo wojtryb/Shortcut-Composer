439a94ba3837707a59e207810e89cd0fba6e0fdb
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Refactoring transform actions

--- shortcut_composer/composer_utils/transform_actions.py
+++ shortcut_composer/composer_utils/transform_actions.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict
+from typing import Dict
 from time import sleep
 from threading import Thread
 from functools import partialmethod
@@ -14,56 +14,54 @@ class TransformModeActions:
 
     def __init__(self) -> None:
         """Remember the reference to toolbox krita object."""
-        self.transform_options: Optional[QWidget] = None
-        self.buttons: Dict[Tool, QToolButton] = {}
-        self.apply_button: Optional[QPushButton] = None
+        self.transform_options: QWidget
+        self.apply_button: QPushButton
+        self.tool_buttons: Dict[Tool, QToolButton] = {}
+        self._initialized = False
 
-    def _late_init(self, tool: Tool):
-        if tool not in self.buttons:
-            self.buttons[tool] = self._fetch_tool_button(tool)
-
-        if self.apply_button is None:
+    def _ensure_initialized(self, tool: Tool):
+        if not self._initialized:
+            Tool.TRANSFORM.activate()
+            self.transform_options = self._fetch_transform_options()
             self.apply_button = self._fetch_apply_button()
+            self._initialized = True
 
-    def _set_button(self, tool: Tool):
-        self._late_init(tool)
+        if tool not in self.tool_buttons:
+            self.tool_buttons[tool] = self._fetch_tool_button(tool)
+
+    def _set_mode(self, tool: Tool) -> None:
+        self._ensure_initialized(tool)
+        button = self.tool_buttons[tool]
 
         if Krita.active_tool == Tool.TRANSFORM:
-            self.apply_button.click()
-            self.buttons[tool].click()
-            return
+            return self._activate_mode_button(button)
 
         Tool.TRANSFORM.activate()
-        args = [self.buttons[tool]]
-        Thread(target=self._delayed_click, args=args, daemon=True).start()
+        Thread(target=self._delayed_click, args=[button], daemon=True).start()
 
     def _delayed_click(self, button: QToolButton):
         sleep(0.1)
+        self._activate_mode_button(button)
+
+    def _activate_mode_button(self, button: QToolButton):
         self.apply_button.click()
         button.click()
 
-    set_perspective = partialmethod(_set_button, Tool.TRANSFORM_PERSPECTIVE)
-    set_warp = partialmethod(_set_button, Tool.TRANSFORM_WARP)
-    set_cage = partialmethod(_set_button, Tool.TRANSFORM_CAGE)
-    set_liquify = partialmethod(_set_button, Tool.TRANSFORM_LIQUIFY)
-    set_mesh = partialmethod(_set_button, Tool.TRANSFORM_MESH)
+    set_perspective = partialmethod(_set_mode, Tool.TRANSFORM_PERSPECTIVE)
+    set_warp = partialmethod(_set_mode, Tool.TRANSFORM_WARP)
+    set_cage = partialmethod(_set_mode, Tool.TRANSFORM_CAGE)
+    set_liquify = partialmethod(_set_mode, Tool.TRANSFORM_LIQUIFY)
+    set_mesh = partialmethod(_set_mode, Tool.TRANSFORM_MESH)
 
     def _fetch_tool_button(self, tool: Tool) -> QToolButton:
-        Tool.TRANSFORM.activate()
-        self.transform_options = self._fetch_transform_options()
-
         for qobj in self.transform_options.findChildren(QToolButton):
             if qobj.objectName() == self._BUTTONS_MAP[tool]:
                 return qobj  # type: ignore
         raise RuntimeError("Couldnt find tool button.")
 
     def _fetch_apply_button(self) -> QPushButton:
-        Tool.TRANSFORM.activate()
-        self.transform_options = self._fetch_transform_options()
-
         for qobj in self.transform_options.findChildren(QPushButton):
             qobj: QPushButton
-            print(qobj.objectName(), qobj.text())
             if qobj.text() == "&Apply":
                 return qobj  # type: ignore
         raise RuntimeError("Couldnt find apply button.")
