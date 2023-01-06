8e07046156a67d1175836aeab679561e1cafc459
wojtryb <wojtryb@gmail.com>
2023-01-06 14:03:39 +0100

    Simpler key event interface as a base for all other interfaces

--- tool_modifiers/shortcut_library/plugin_actions/_interfaces.py
+++ tool_modifiers/shortcut_library/plugin_actions/_interfaces.py
@@ -7,21 +7,53 @@ class PluginAction(ABC):
 
     action_name: str
 
-    def set_low(self):
+    def on_key_press(self):
         pass
 
-    def set_high(self):
+    def on_long_key_release(self):
         pass
 
-    def is_high_state(self):
+    def on_short_key_release(self):
         pass
+
+    def on_every_key_release(self):
+        pass
+
+
+class TemporaryAction(PluginAction):
+
+    action_name: str
+
+    @abstractmethod
+    def _set_low(self):
+        pass
+
+    @abstractmethod
+    def _set_high(self):
+        pass
+
+    @abstractmethod
+    def _is_high_state(self):
+        pass
+
+    def on_key_press(self):
+        self._state_before_press = self._is_high_state()
+        if not self._state_before_press:
+            self._set_high()
+
+    def on_short_key_release(self):
+        if self._state_before_press:
+            self._set_low()
+
+    def on_long_key_release(self):
+        self._set_low()
 
 
 class CyclicPluginAction(PluginAction):
 
     action_name: str
-    values: List[str]
-    default_value: str
+    _values_to_cycle: List[str]
+    _default_value: str
 
     @abstractmethod
     def _set_value(self, value: str) -> None:
@@ -31,22 +63,19 @@ class CyclicPluginAction(PluginAction):
     def _get_current_value(self) -> str:
         pass
 
-    def set_low(self):
-        self._set_value(self.default_value)
-
-    def set_high(self):
+    def on_key_press(self):
         current_value = self._get_current_value()
-        if current_value not in self.values:
-            self._set_value(self.values[0])
+        if current_value not in self._values_to_cycle:
+            self._set_value(self._values_to_cycle[0])
             return
 
         for tool, next_tool in zip_longest(
-                self.values,
-                self.values[1:],
-                fillvalue=self.values[0]):
+                self._values_to_cycle,
+                self._values_to_cycle[1:],
+                fillvalue=self._values_to_cycle[0]):
             if tool == current_value:
                 self._set_value(next_tool)
                 return
 
-    def is_high_state(self):
-        return False
+    def on_long_key_release(self):
+        self._set_value(self._default_value)
