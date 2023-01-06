c3d295904081c01056fe9c44c590342cb8f421e4
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-06 14:03:40 +0100

    Handle collapsing and current visibility in layer picker

--- shortcut_composer/composer_library/templates/layer_picker.py
+++ shortcut_composer/composer_library/templates/layer_picker.py
@@ -20,8 +20,17 @@ class PickStrategy(Enum):
         return [node for node in nodes
                 if node.is_visible() or node == current_node]
 
+    @staticmethod
+    def __pick_current_visibility(document: Document) -> List[Node]:
+        nodes = document.all_nodes()
+        current_node = document.current_node()
+        current_visibility = current_node.is_visible()
+        return [node for node in nodes
+                if node.is_visible() == current_visibility]
+
     ALL = __pick_all
     VISIBLE = __pick_visible
+    CURRENT_VISIBILITY = __pick_current_visibility
 
 
 class LayerPicker(SingleAxisTracker):
