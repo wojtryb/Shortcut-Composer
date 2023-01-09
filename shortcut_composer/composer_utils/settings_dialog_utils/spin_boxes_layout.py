45bb98e894951045dc77d3a4ea2d6f0a71d432fa
Wojciech Trybus <wtrybus@ess.etisoft.pl>
2023-01-09 09:18:00 +0100

    Fixed PEP8 mistakes in SpinBoxesLayout

--- shortcut_composer/composer_utils/settings_dialog_utils/spin_boxes_layout.py
+++ shortcut_composer/composer_utils/settings_dialog_utils/spin_boxes_layout.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from typing import Dict, Union
+from dataclasses import dataclass
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QDoubleSpinBox,
@@ -19,27 +20,78 @@ SpinBox = Union[QSpinBox, QDoubleSpinBox]
 class SpinBoxesLayout(QFormLayout):
     """Dialog zone consisting of spin boxes."""
 
+    @dataclass
+    class ConfigParams:
+        """Adds spinbox parametrization to the config field."""
+        config: Config
+        step: float
+        max_value: float
+        is_int: bool
+
     def __init__(self) -> None:
         super().__init__()
         self._forms: Dict[Config, SpinBox] = {}
 
         self._add_label("Common settings")
-        self._add_row(Config.SHORT_VS_LONG_PRESS_TIME, step=0.05, max_value=5, is_int=False)
-        self._add_row(Config.FPS_LIMIT, step=10, max_value=300, is_int=True)
+
+        self._add_row(self.ConfigParams(
+            Config.SHORT_VS_LONG_PRESS_TIME,
+            step=0.05,
+            max_value=4,
+            is_int=False))
+
+        self._add_row(self.ConfigParams(
+            Config.FPS_LIMIT,
+            step=5,
+            max_value=300,
+            is_int=True))
 
         self._add_label("Cursor trackers")
-        self._add_row(Config.TRACKER_SENSITIVITY_SCALE, step=0.05, max_value=10, is_int=False)
-        self._add_row(Config.TRACKER_DEADZONE, step=1, max_value=99, is_int=True)
+
+        self._add_row(self.ConfigParams(
+            Config.TRACKER_SENSITIVITY_SCALE,
+            step=0.05,
+            max_value=4,
+            is_int=False))
+
+        self._add_row(self.ConfigParams(
+            Config.TRACKER_DEADZONE,
+            step=1,
+            max_value=200,
+            is_int=True))
 
         self._add_label("Pie menus display")
-        self._add_row(Config.PIE_GLOBAL_SCALE, step=0.05, max_value=10, is_int=False)
-        self._add_row(Config.PIE_ICON_GLOBAL_SCALE, step=0.05, max_value=10, is_int=False)
-        self._add_row(Config.PIE_DEADZONE_GLOBAL_SCALE, step=0.05, max_value=10, is_int=False)
-        self._add_row(Config.PIE_ANIMATION_TIME, step=0.05, max_value=10, is_int=False)
 
-    def _add_row(self, config: Config, step: float, max_value: float, is_int: bool) -> None:
+        self._add_row(self.ConfigParams(
+            Config.PIE_GLOBAL_SCALE,
+            step=0.05,
+            max_value=4,
+            is_int=False))
+
+        self._add_row(self.ConfigParams(
+            Config.PIE_ICON_GLOBAL_SCALE,
+            step=0.05,
+            max_value=4,
+            is_int=False))
+
+        self._add_row(self.ConfigParams(
+            Config.PIE_DEADZONE_GLOBAL_SCALE,
+            step=0.05,
+            max_value=4,
+            is_int=False))
+
+        self._add_row(self.ConfigParams(
+            Config.PIE_ANIMATION_TIME,
+            step=0.01,
+            max_value=1,
+            is_int=False))
+
+    def _add_row(self, config_params: ConfigParams) -> None:
         """Add a spin box to the layout along with its description."""
-        self.addRow(config.value, self._create_form(config, step, max_value, is_int))
+        self.addRow(
+            config_params.config.value,
+            self._create_form(config_params)
+        )
 
     def _add_label(self, text: str):
         label = QLabel(text)
@@ -47,15 +99,15 @@ class SpinBoxesLayout(QFormLayout):
         self.addRow(QSplitter(Qt.Horizontal))
         self.addRow(label)
 
-    def _create_form(self, config: Config, step: float, max_value: float, is_int: bool) -> SpinBox:
+    def _create_form(self, config_params: ConfigParams) -> SpinBox:
         """Store and return new spin box for required type (int or float)."""
-        form = QSpinBox() if is_int else QDoubleSpinBox()
-        form.setObjectName(config.value)
+        form = QSpinBox() if config_params.is_int else QDoubleSpinBox()
+        form.setObjectName(config_params.config.value)
         form.setMinimum(0)
-        form.setSingleStep(step)  # type: ignore
-        form.setMaximum(max_value)
+        form.setMaximum(config_params.max_value)  # type: ignore
+        form.setSingleStep(config_params.step)  # type: ignore
 
-        self._forms[config] = form
+        self._forms[config_params.config] = form
         return form
 
     def refresh(self) -> None:
