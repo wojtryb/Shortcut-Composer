6ad5bb568de029898ef0f41b0607427bcf4ba986
Kolloom <31672176+thompson-vii@users.noreply.github.com>
2023-01-09 01:52:09 -0600

    Allow different parametrization for each configuration value (#5)
    * Raise fps limit and set stepsize to 10
    
    * Expose SpinBoxesLayout to more options
    
    Added step size and max_value
    
    Co-authored-by: Kolloom <fivshme.hotmail.com>

--- shortcut_composer/composer_utils/settings_dialog_utils/spin_boxes_layout.py
+++ shortcut_composer/composer_utils/settings_dialog_utils/spin_boxes_layout.py
@@ -24,22 +24,22 @@ class SpinBoxesLayout(QFormLayout):
         self._forms: Dict[Config, SpinBox] = {}
 
         self._add_label("Common settings")
-        self._add_row(Config.SHORT_VS_LONG_PRESS_TIME, is_int=False)
-        self._add_row(Config.FPS_LIMIT, is_int=True)
+        self._add_row(Config.SHORT_VS_LONG_PRESS_TIME, step=0.05, max_value=5, is_int=False)
+        self._add_row(Config.FPS_LIMIT, step=10, max_value=300, is_int=True)
 
         self._add_label("Cursor trackers")
-        self._add_row(Config.TRACKER_SENSITIVITY_SCALE, is_int=False)
-        self._add_row(Config.TRACKER_DEADZONE, is_int=True)
+        self._add_row(Config.TRACKER_SENSITIVITY_SCALE, step=0.05, max_value=10, is_int=False)
+        self._add_row(Config.TRACKER_DEADZONE, step=1, max_value=99, is_int=True)
 
         self._add_label("Pie menus display")
-        self._add_row(Config.PIE_GLOBAL_SCALE, is_int=False)
-        self._add_row(Config.PIE_ICON_GLOBAL_SCALE, is_int=False)
-        self._add_row(Config.PIE_DEADZONE_GLOBAL_SCALE, is_int=False)
-        self._add_row(Config.PIE_ANIMATION_TIME, is_int=False)
+        self._add_row(Config.PIE_GLOBAL_SCALE, step=0.05, max_value=10, is_int=False)
+        self._add_row(Config.PIE_ICON_GLOBAL_SCALE, step=0.05, max_value=10, is_int=False)
+        self._add_row(Config.PIE_DEADZONE_GLOBAL_SCALE, step=0.05, max_value=10, is_int=False)
+        self._add_row(Config.PIE_ANIMATION_TIME, step=0.05, max_value=10, is_int=False)
 
-    def _add_row(self, config: Config, is_int: bool) -> None:
-        """Add a spin box to the layout along with its desctiption."""
-        self.addRow(config.value, self._create_form(config, is_int))
+    def _add_row(self, config: Config, step: float, max_value: float, is_int: bool) -> None:
+        """Add a spin box to the layout along with its description."""
+        self.addRow(config.value, self._create_form(config, step, max_value, is_int))
 
     def _add_label(self, text: str):
         label = QLabel(text)
@@ -47,12 +47,13 @@ class SpinBoxesLayout(QFormLayout):
         self.addRow(QSplitter(Qt.Horizontal))
         self.addRow(label)
 
-    def _create_form(self, config: Config, is_int: bool) -> SpinBox:
+    def _create_form(self, config: Config, step: float, max_value: float, is_int: bool) -> SpinBox:
         """Store and return new spin box for required type (int or float)."""
         form = QSpinBox() if is_int else QDoubleSpinBox()
         form.setObjectName(config.value)
         form.setMinimum(0)
-        form.setSingleStep(1 if is_int else 0.05)  # type: ignore
+        form.setSingleStep(step)  # type: ignore
+        form.setMaximum(max_value)
 
         self._forms[config] = form
         return form
