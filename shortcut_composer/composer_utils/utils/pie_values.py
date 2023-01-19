e7d97cdb7bd1c80af7cb5263049cb8bc0b2b82a8
Wojciech Trybus <wojtryb@gmail.com>
2023-01-19 21:25:20 +0100

    Picking values with two lists - needs refactor

--- shortcut_composer/composer_utils/utils/pie_values.py
+++ shortcut_composer/composer_utils/utils/pie_values.py
@@ -3,12 +3,13 @@
 
 from typing import Set
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QVBoxLayout,
     QPushButton,
     QHBoxLayout,
-    QComboBox,
     QWidget,
+    QLabel,
 )
 
 from api_krita import Krita
@@ -19,46 +20,69 @@ from .value_list import ValueList
 class PieValues(QWidget):
     def __init__(self, allowed_values: Set[str], config: Config) -> None:
         super().__init__()
-        layout = QVBoxLayout()
-        self.allowed_values = sorted(allowed_values)
+        layout = QHBoxLayout()
+        self.allowed_values = set(allowed_values)
         self.config = config
 
-        self.list_widget = ValueList(self)
-
-        self.combo_box = QComboBox()
-        self.combo_box.addItems(self.allowed_values)
+        self.available_list = ValueList(movable=False, parent=self)
+        self.current_list = ValueList(movable=True, parent=self)
 
         add_button = QPushButton(Krita.get_icon("list-add"), "")
-        add_button.setFixedWidth(40)
+        add_button.setStyleSheet("background-color : green")
+        add_button.setFixedHeight(add_button.sizeHint().height()*3)
         add_button.clicked.connect(self.add)
 
         remove_button = QPushButton(Krita.get_icon("deletelayer"), "")
-        remove_button.setFixedWidth(40)
-        remove_button.clicked.connect(self.list_widget.remove_selected)
+        remove_button.setStyleSheet("background-color : red")
+        remove_button.setFixedHeight(remove_button.sizeHint().height()*3)
+        remove_button.clicked.connect(self.remove)
 
-        control_layout = QHBoxLayout()
-        control_layout.addWidget(self.combo_box)
+        control_layout = QVBoxLayout()
+        control_layout.addStretch()
         control_layout.addWidget(add_button)
         control_layout.addWidget(remove_button)
+        control_layout.addStretch()
 
+        layout.addLayout(self._labeled_list(self.available_list, "Available:"))
         layout.addLayout(control_layout)
-        layout.addWidget(self.list_widget)
+        layout.addLayout(self._labeled_list(self.current_list, "Selected:"))
 
         self.setLayout(layout)
 
+    def _labeled_list(self, value_list: ValueList, text: str) -> QVBoxLayout:
+        layout = QVBoxLayout()
+        label = QLabel(text)
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+        layout.addWidget(value_list)
+        return layout
+
     def add(self):
-        self.list_widget.insert(
-            position=self.list_widget.current_row,
-            value=self.combo_box.currentText()
-        )
+        for item in self.available_list.selected:
+            self.current_list.insert(
+                position=self.current_list.current_row,
+                value=item.text(),
+            )
+            self.available_list.remove(name=item.text(),)
+
+    def remove(self):
+        selected = [item.text() for item in self.current_list.selected]
+        self.current_list.remove_selected()
+        new_available = set(self.available_list.get_all()) | set(selected)
+        self.available_list.clear()
+        self.available_list.addItems(sorted(new_available))
 
     def apply(self):
         texts = []
-        for row in range(self.list_widget.count()):
-            texts.append(self.list_widget.item(row).text())
+        for row in range(self.current_list.count()):
+            texts.append(self.current_list.item(row).text())
         self.config.write(";".join(texts))
 
     def refresh(self):
-        self.list_widget.clear()
+        self.current_list.clear()
         currently_set: str = self.config.read()
-        self.list_widget.addItems(currently_set.split(";"))
+        self.current_list.addItems(currently_set.split(";"))
+
+        self.available_list.clear()
+        allowed_items = sorted(self.allowed_values - set(currently_set))
+        self.available_list.addItems(allowed_items)
--- shortcut_composer/composer_utils/utils/value_list.py
+++ shortcut_composer/composer_utils/utils/value_list.py
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: © 2022 Wojciech Trybus <wojtryb@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from typing import Optional
+from typing import Optional, List
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QAbstractItemView,
     QListWidget,
@@ -11,10 +12,13 @@ from PyQt5.QtWidgets import (
 
 
 class ValueList(QListWidget):
-    def __init__(self, parent: Optional[QWidget] = None) -> None:
+    def __init__(self, movable: bool, parent: Optional[QWidget] = None):
         super().__init__(parent)
-        self.setDragDropMode(QAbstractItemView.InternalMove)
+        self.horizontalScrollBar().setStyleSheet("QScrollBar {height:0px;}")
+        self.setMaximumWidth(200)
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        if movable:
+            self.setDragDropMode(QAbstractItemView.InternalMove)
 
     @property
     def current_row(self):
@@ -26,6 +30,21 @@ class ValueList(QListWidget):
         self.insertItem(position+1, value)
         self.clearSelection()
         self.setCurrentRow(position+1)
+
+    def get_all(self):
+        items: List[str] = []
+        for i in range(self.count()):
+            items.append(self.item(i).text())
+        return items
+
+    @property
+    def selected(self):
+        selected = self.selectedIndexes()
+        return [self.item(item.row()) for item in selected]
+
+    def remove(self, name: str):
+        for item in self.findItems(name, Qt.MatchExactly):
+            self.takeItem(self.row(item))
 
     def remove_selected(self):
         selected = self.selectedIndexes()
