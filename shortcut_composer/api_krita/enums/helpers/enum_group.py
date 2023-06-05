f8c6bca1ba078baf98296e91837b0664fb02f631
Wojciech Trybus <wojtryb@gmail.com>
2023-06-05 11:25:23 +0200

    Rewrite EnumGroupMetaclass in a way which works better with typing

--- shortcut_composer/api_krita/enums/helpers/enum_group.py
+++ shortcut_composer/api_krita/enums/helpers/enum_group.py
@@ -1,9 +1,50 @@
-from typing import Any, Dict, Set, Tuple, Type
+from typing import Any, Dict, Set, Iterable, Tuple, Type, Generic, TypeVar
 from enum import Enum
+T = TypeVar("T", bound=Enum)
 
 
-class EnumGroupMetaclass(type):
-    """Metaclass for creating enum groups. See EnumGroup documentation."""
+class EnumGroupMeta(type, Generic[T]):
+    """
+    Metaclass for creating enum groups.
+
+    Intended use is to place enum definitions inside a class definition:
+    ```
+    class EnumGroup(metaclass=EnumGroupMeta[Enum]):
+        class EnumA(Enum):
+            ASD = "asd"
+            QWE = "qwe"
+
+        class EnumB(Enum):
+            Z = "z"
+            X = "x"
+    ```
+    Metaclass makes its instance act as a composite of all the grouped
+    enums. Although the instance technically is not an Enum subclass,
+    it provides the following Enum class attributes:
+    - `_member_map_`
+    - `_value2member_map_`
+
+    Enum members can be fetched in multiple ways:
+        - Using the original, grouped Enum class:
+            - EnumGroup.EnumA.ASD
+            - EnumGroup.EnumA["ASD"]
+            - EnumGroup.EnumA("asd")
+
+        - Using the composite class directly:
+            - EnumGroup.ASD
+            - EnumGroup["ASD"]
+            - EnumGroup("asd")
+
+    Grouped enum types must follow those rules:
+    - All passed enum types must inherit directly from the same Enum.
+    - Grouped enums cannot define the same names.
+
+    Breaking those rules will result in exception during class creation.
+
+    Grouped enums do not need to directly inherit from Enum.
+    Metaclass is a Generic - for typing purposes, base class of grouped
+    enum types should also be passed to the metaclass.
+    """
 
     def __init__(
         self,
@@ -13,15 +54,31 @@ class EnumGroupMetaclass(type):
     ) -> None:
         super().__init__(name, bases, attrs)
 
-        self._enums_ = MetaclassTools.get_enum_types(attrs)
-        """Dictionary mapping enum types to their names."""
+        self._grouped_enums_ = MetaInit.get_enum_types(attrs)
+        """Dictionary mapping grouped enum types to their names."""
+        MetaInit.ensure_common_base(self._grouped_enums_.values())
 
-        self._compound_ = MetaclassTools.create_compound(self._enums_)
-        """Enum containing elements of all grouped enums."""
+        self._member_map_ = MetaInit.init_member_map(self._grouped_enums_)
+        """Dictionary mapping enum members to their names."""
+
+        self._value2member_map_ = MetaInit.init_reverse_map(self._member_map_)
+        """Dictionary mapping enum members to their values."""
+
+    def __getattr__(self, name: str) -> T:
+        """Return enum member as attribute of the composite."""
+        return self._member_map_[name]
+
+    def __getitem__(self, name: str) -> T:
+        """Return enum member by its name."""
+        return self._member_map_[name]
+
+    def __call__(self, value: str) -> T:
+        """Return enum member by its value."""
+        return self._value2member_map_[value]
 
 
-class MetaclassTools:
-    """Organizes methods used by EnumGroupMetaclass."""
+class MetaInit:
+    """Organizes methods used by EnumGroupMeta."""
 
     @staticmethod
     def get_enum_types(attrs: Dict[str, Any]) -> Dict[str, Type[Enum]]:
@@ -33,23 +90,30 @@ class MetaclassTools:
         return {name: val for name, val in attrs.items() if is_enum_type(val)}
 
     @classmethod
-    def create_compound(cls, enum_types: Dict[str, Type[Enum]]) -> Type[Enum]:
-        """Combine passed enums types into single enum."""
+    def init_member_map(cls, enum_types: Dict[str, Type[Enum]]):
+        """Return dict mapping enum members to their names."""
         # maps enum values to their names from all the passed types
-        compound_attrs = {}
+        member_map = {}
         for enum in enum_types.values():
 
             # raise an error when two enum types define the same name
-            if common := cls.common_keys(compound_attrs, enum._member_map_):
+            if common := cls.common_keys(member_map, enum._member_map_):
                 raise RuntimeError(f"Enums have repeated keys: {common}")
 
             # update dictionary
-            args = {name: val.value for name, val in enum._member_map_.items()}
-            compound_attrs.update(args)
+            args = {name: val for name, val in enum._member_map_.items()}
+            member_map.update(args)
 
-        # dynamically create enum composite keeping the same base class
-        enum_base = cls.get_enum_base(enum_types)
-        return enum_base("Compound", compound_attrs)
+        return member_map
+
+    @staticmethod
+    def init_reverse_map(member_map: dict):
+        """Return dict mapping enum members to their values."""
+        reverse_map = {}
+        for value in member_map.values():
+            if value not in reverse_map.values():
+                reverse_map[value.value] = value
+        return reverse_map
 
     @staticmethod
     def common_keys(dict_1: dict, dict_2: dict) -> Set:
@@ -59,44 +123,12 @@ class MetaclassTools:
         return keys_1.intersection(keys_2)
 
     @staticmethod
-    def get_enum_base(enum_types: Dict[str, Type[Enum]]) -> Type[Enum]:
+    def ensure_common_base(enum_types: Iterable[Type[Enum]]) -> None:
         """
         Return common base class of given enum types.
 
         Raise exception when their bases are not exactly the same.
         """
-        enum_bases = [enum.__base__ for enum in enum_types.values()]
+        enum_bases = [enum.__base__ for enum in enum_types]
         if not enum_bases[:-1] == enum_bases[1:]:
             raise RuntimeError("All enums must have the same base.")
-        return enum_bases[0] if enum_bases else Enum
-
-
-class EnumGroup(metaclass=EnumGroupMetaclass):
-    """
-    Base class for creating enum groups.
-
-    Intended use is to place enum definitions inside a subclass:
-    ```
-    class CustomEnumGroup(EnumGroup):
-        class EnumA(Enum):
-            ASD = "asd"
-            QWE = "qwe"
-
-        class EnumB(Enum):
-            Z = "z"
-            X = "x"
-    ```
-    Child class automatically provides following class attributes:
-    - `_compound_` - dynamically created enum containing elements of
-      all grouped enums.
-    - `_enums_` - dictionary mapping enum types to their names.
-
-    Grouped enums do not need to directly inherit from Enum. If subclass
-    of enum is used, the composite will also be based on the same class.
-
-    Grouped enum types must follow those rules:
-    - All passed enum types must inherit directly from the same class.
-    - Enums cannot define the same names.
-
-    Breaking those rules will result in exception during class creation.
-    """
