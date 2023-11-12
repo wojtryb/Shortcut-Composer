a526c9c45247a635eadad803bf7368564b764ea9
Wojciech Trybus <wojtryb@gmail.com>
2023-11-12 12:30:11 +0100

    Bump plugin version to 1.4.2

--- shortcut_composer/INFO.py
+++ shortcut_composer/INFO.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: © 2022-2023 Wojciech Trybus <wojtryb@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-__version__ = "1.4.2.dev"
+__version__ = "1.4.2"
 __author__ = "Wojciech Trybus"
 __license__ = "GPL-3.0-or-later"
