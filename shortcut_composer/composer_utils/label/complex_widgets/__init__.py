11b7c7a05310a5d03f7bb1ad855df2ebacdacb60
Wojciech Trybus <wojtryb@gmail.com>
2023-11-16 12:06:19 +0100

    Move NumericValuePicker to label_complex_widgets

--- shortcut_composer/composer_utils/label/complex_widgets/__init__.py
+++ shortcut_composer/composer_utils/label/complex_widgets/__init__.py
@@ -1,3 +1,4 @@
 from .scroll_area import ScrollArea
+from .numeric_value_picker import NumericValuePicker
 
-__all__ = ["ScrollArea"]
+__all__ = ["ScrollArea", "NumericValuePicker"]
--- shortcut_composer/templates/pie_menu_utils/pie_settings_impl/numeric_pie_settings.py
+++ shortcut_composer/templates/pie_menu_utils/pie_settings_impl/numeric_pie_settings.py
@@ -1,98 +1,13 @@
 # SPDX-FileCopyrightText: © 2022-2023 Wojciech Trybus <wojtryb@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (
-    QAbstractSpinBox,
-    QHBoxLayout,
-    QVBoxLayout,
-    QScrollArea,
-    QSpinBox,
-    QWidget,
-    QLabel)
-
 from core_components import NumericController
-from composer_utils.label import LabelWidgetStyle
-from composer_utils.label.label_widget_impl import dispatch_label_widget
+from composer_utils.label.complex_widgets import NumericValuePicker
+
 from templates.pie_menu_utils.pie_config_impl import NonPresetPieConfig
 from templates.pie_menu_utils import PieSettings
 from ..style_holder import StyleHolder
 from ..pie_label import PieLabel
-
-
-class NumericValuePicker(QWidget):
-    def __init__(
-        self,
-        controller: NumericController,
-        unscaled_label_style: LabelWidgetStyle,
-        parent=None
-    ) -> None:
-        super().__init__(parent)
-
-        self._controller = controller
-        self._unscaled_label_style = unscaled_label_style
-
-        self._icon = self._create_icon(value=0)
-        self._spin_box = self._init_spin_box()
-        self._icon_holder = self._init_icon_holder()
-        self._layout = self._init_layout()
-
-        self.setLayout(self._layout)
-
-    def _init_layout(self):
-        right_side = QVBoxLayout()
-        right_side.setAlignment(Qt.AlignTop)
-        right_side.addWidget(QLabel("\nSet icon value:"))
-        right_side.addWidget(self._spin_box)
-
-        layout = QHBoxLayout()
-        layout.setAlignment(Qt.AlignTop)
-        layout.addWidget(self._icon_holder, 1, Qt.AlignTop)
-        layout.addLayout(right_side, 1)
-
-        return layout
-
-    def _create_icon(self, value: int):
-        label = PieLabel.from_value(value, self._controller)
-
-        if label is None:
-            raise Exception
-
-        icon = dispatch_label_widget(label)(
-            label=label,
-            label_widget_style=self._unscaled_label_style,
-            parent=self)
-        icon.setFixedSize(icon.icon_radius*2, icon.icon_radius*2)
-        icon.draggable = True
-
-        return icon
-
-    def _init_spin_box(self):
-        def update_icon(a0: int):
-            self._icon = self._create_icon(a0)
-            self._icon_holder.setWidget(self._icon)
-
-        spin_box = QSpinBox()
-        spin_box.setMinimum(self._controller.MIN_VALUE)
-        spin_box.setMaximum(self._controller.MAX_VALUE)
-        spin_box.setSingleStep(self._controller.STEP)
-        spin_box.setWrapping(self._controller.WRAPPING)
-        if self._controller.ADAPTIVE:
-            spin_box.setStepType(QAbstractSpinBox.AdaptiveDecimalStepType)
-        spin_box.valueChanged.connect(update_icon)
-        return spin_box
-
-    def _init_icon_holder(self):
-        icon_holder = QScrollArea()
-        icon_holder.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        icon_holder.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        icon_holder.setWidget(self._icon)
-        icon_holder.setAlignment(Qt.AlignCenter)
-        icon_holder.setSizeAdjustPolicy(QScrollArea.AdjustIgnored)
-        icon_holder.setFixedSize(
-            round(self._icon.width()*1.1),
-            round(self._icon.height()*1.1))
-        return icon_holder
 
 
 class NumericPieSettings(PieSettings):
@@ -107,9 +22,20 @@ class NumericPieSettings(PieSettings):
     ) -> None:
         super().__init__(config, style_holder)
 
+        def label_from_integer(value: int) -> PieLabel[int]:
+            label = PieLabel.from_value(value, controller)
+            if label is None:
+                raise RuntimeError(f"Could not create label from {value}")
+            return label
+
         self._numeric_picker = NumericValuePicker(
-            controller,
-            style_holder.unscaled_label_style)
+            create_label_from_integer=label_from_integer,
+            unscaled_label_style=style_holder.unscaled_label_style,
+            min_value=controller.MIN_VALUE,
+            max_value=controller.MAX_VALUE,
+            step=controller.STEP,
+            wrapping=controller.WRAPPING,
+            adaptive=controller.ADAPTIVE)
 
         self._tab_holder.insertTab(1, self._numeric_picker, "Values")
         self._tab_holder.setCurrentIndex(1)
