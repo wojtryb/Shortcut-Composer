13eab11994848c4be5342296609a4868db23099e
Wojciech Trybus <wojtryb@gmail.com>
2024-03-22 17:11:42 +0100

    Handle errors when parsing krita version

--- shortcut_composer/api_krita/wrappers/version.py
+++ shortcut_composer/api_krita/wrappers/version.py
@@ -18,6 +18,9 @@ class Version:
     additional_info: str = ""
 
     def __lt__(self, other: 'Version') -> bool:
+        if isinstance(other, UnknownVersion):
+            return True
+
         def version_as_int(version: Version) -> int:
             return version.major*1_000_000 + version.minor*1_000 + version.fix
 
@@ -27,3 +30,17 @@ class Version:
         if not self.additional_info:
             return f"{self.major}.{self.minor}.{self.fix}"
         return f"{self.major}.{self.minor}.{self.fix}-{self.additional_info}"
+
+
+class UnknownVersion(Version):
+    """Represents unknown application version."""
+
+    def __init__(self) -> None:
+        super().__init__(0, 0, 0, "unknown")
+
+    def __lt__(self, other: 'Version') -> bool:
+        """Consider unknown version to be higher than any valid one."""
+        return False
+
+    def __str__(self) -> str:
+        return "unknown"
